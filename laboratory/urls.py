9fd90250c2c7d39219d7c3fbf582979f5de64465
TaylorHomes <taylor.homes01@gmail.com>
2019-03-28 23:18:33 -0400

    box is set up, now to throw around some sand

--- laboratory/urls.py
+++ laboratory/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from core import views
 
 urlpatterns = [
+    path('', views.index, name='home'),
+    path(
+        'contact/',
+        TemplateView.as_view(template_name='contact.html'),
+        name='contact'),
+    path(
+        'about/',
+        TemplateView.as_view(template_name='about.html'),
+        name='about'),
     path('admin/', admin.site.urls),
 ]
